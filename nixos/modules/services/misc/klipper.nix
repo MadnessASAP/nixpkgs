faec3380bd5a4d3379c6a2e725dde194fefba6a7
Michael 'ASAP' Weinrich <michael@a5ap.net>
2024-04-14 14:55:56 -0700

    mostly working

--- nixos/modules/services/misc/klipper.nix
+++ nixos/modules/services/misc/klipper.nix
@@ -9,7 +9,43 @@ let
       else lib.concatMapStrings (s: "\n  ${generators.mkValueStringDefault {} s}") l;
     mkKeyValue = generators.mkKeyValueDefault { } ":";
   };
-  mutable = cfg.mutableConfig || (cfg.mutableSettings != null);
+  flattenConfig =
+    let
+      attrsToFlatten = [ "include" ];
+      flattenAttr = prfx: attr: pipe attr [
+        # convert lists to empty attrsets
+        (v:
+          if (isList v)
+          then genAttrs v (_: { })
+          else v
+        )
+        # prefix attribute names
+        (mapAttrs'
+          (name: nameValuePair "${prfx} ${name}")
+        )
+      ];
+    in
+    input: pipe attrsToFlatten [
+      # build a list of flattend attributes
+      (map
+        (prfx: flattenAttr prfx (input.${prfx} or { }))
+      )
+      # merge the list of flattened attributes with input
+      (attrList: attrsets.mergeAttrsList (attrList ++ [ input ]))
+      # remove flattened attributes
+      (filterAttrs
+        (name: _: ! (elem name attrsToFlatten))
+      )
+    ];
+  baseServiceConfig = {
+    StateDirectory = "klipper";
+    DynamicUser = true;
+    User = "klipper";
+  } // (optionalAttrs (cfg.user != null) {
+    DynamicUser = false;
+    User = cfg.user;
+    Group = cfg.group;
+  });
 in
 {
   ##### interface
@@ -41,11 +77,60 @@ in
         description = "Path of the API socket to create.";
       };
 
+      apiServer = {
+        enable = mkEnableOption "Moonraker API server";
+
+        address = mkOption {
+          type = types.nullOr types.str;
+          default = "127.0.0.1";
+          example = "0.0.0.0";
+          description = "The IP or host to listen on.";
+        };
+
+
+        port = mkOption {
+          type = types.port;
+          default = 7125;
+          description = "The port to listen on.";
+        };
+
+        allowSystemControl = mkOption {
+          type = types.bool;
+          default = false;
+          description = ''
+            Whether to allow Moonraker to perform system-level operations.
+
+            Moonraker exposes APIs to perform system-level operations, such as
+            reboot, shutdown, and management of systemd units. See the
+            [documentation](https://moonraker.readthedocs.io/en/latest/web_api/#machine-commands)
+            for details on what clients are able to do.
+          '';
+        };
+
+        settings = mkOption {
+          type = format.type;
+          default = { };
+          example = {
+            authorization = {
+              trusted_clients = [ "10.0.0.0/24" ];
+              cors_domains = [ "https://app.fluidd.xyz" "https://my.mainsail.xyz" ];
+            };
+          };
+          description = ''
+            Configuration for Moonraker. See the [documentation](https://moonraker.readthedocs.io/en/latest/configuration/)
+            for supported values.
+          '';
+        };
+
+        octoprintCompat = mkEnableOption "Octoprint compatability";
+      };
+
       # DEPRECATED use mutableSettings
       mutableConfig = mkOption {
         type = types.bool;
         default = false;
         example = true;
+        visible = false;
         description = ''
           Whether to copy the config to a mutable directory instead of using the one directly from the nix store.
           This will only copy the config if the file at `services.klipper.mutableConfigPath` doesn't exist.
@@ -56,6 +141,16 @@ in
         type = types.path;
         default = "/var/lib/klipper";
         description = "Path to copy mutable Klipper config file to.";
+      };
+
+      immutableConfigPath = mkOption {
+        type = types.path;
+        default = "${cfg.mutableConfigFolder}/printer-immutable.cfg";
+        description = ''
+          Path to link to immutable config generated from settings.
+          
+          This will be automatically included in the mutable config file.
+        '';
       };
 
       configFile = mkOption {
@@ -93,7 +188,18 @@ in
       };
 
       settings = mkOption {
-        type = types.nullOr format.type;
+        type = types.submodule {
+          freeformType = format.type;
+          options = {
+            include = mkOption {
+              type = types.listOf types.path;
+              default = [ ];
+              description = lib.mdDoc ''
+                List of paths to be included in the klipper config
+              '';
+            };
+          };
+        };
         default = null;
         description = ''
           Configuration for Klipper. See the [documentation](https://www.klipper3d.org/Overview.html#configuration-and-tuning-guides)
@@ -102,27 +208,11 @@ in
       };
 
       mutableSettings = mkOption {
-        type = types.nullOr format.type;
-        default = null;
+        type = format.type;
+        default = { };
         description = lib.mdDoc ''
           Mutable configuration for Klipper. See the [documentation](https://www.klipper3d.org/Overview.html#configuration-and-tuning-guides)
           for supported values. These settings will be copied to klippers config directory if it doesn't already exist.
-        '';
-      };
-
-      mergedSettings = mkOption {
-        type = format.type;
-        default = (
-          attrsets.mergeAttrsList
-            (filter
-              (attr: attr != null)
-              [ cfg.mutableSettings cfg.settings ]
-            )
-        );
-        readOnly = true;
-        visible = false;
-        description = lib.mdDoc ''
-          Read-only merged attrset of mutableSettings and settings
         '';
       };
 
@@ -167,12 +257,16 @@ in
           message = "Option services.klipper.group is not set when services.klipper.user is specified.";
         }
         {
-          assertion = (cfg.configFile == null) -> (cfg.mergedSettings != { });
-          message = "You need to either specify at least one of services.klipper.configFile or at lease one of services.klipper.settings or services.klipper.mutableSettings";
+          assertion = (cfg.configFile != null) || (cfg.settings != null) || (cfg.mutableSettings != null);
+          message = "You need to either specify at least one of services.klipper.configFile, services.klipper.settings, or services.klipper.mutableSettings";
         }
         {
-          assertion = cfg.mutableConfig -> (cfg.mutableSettings == null);
-          message = "services.klipper.mutableSettings cannot be used at the same time as mutableConfig. You may use one OR the other. Preferrably mutableSettings.";
+          assertion = (cfg.apiServer.allowSystemControl -> config.security.polkit.enable);
+          message = "services.klipper.apiServer.allowSystemControl requires security.polkit.enable = true";
+        }
+        {
+          assertion = (cfg.apiServer.allowSystemControl -> cfg.user != null);
+          message = "services.klipper.apiServer.allowSystemControl requires a statically allocated user. Set services.klipper.user to a username defines in users.users.";
         }
       ]
       ++ mapAttrsToList
@@ -184,7 +278,7 @@ in
         })
         cfg.firmwares;
 
-      warnings = (optional cfg.mutableConfig "Option services.klipper.mutableConfig is deprecated. Use mutableSettings instead");
+      warnings = (optional cfg.mutableConfig "Option services.klipper.mutableConfig is deprecated, use mutableSettings instead.");
 
       systemd.services.klipper =
         let
@@ -192,39 +286,42 @@ in
             + optionalString (cfg.apiSocket != null) " --api-server=${cfg.apiSocket}"
             + optionalString (cfg.logFile != null) " --logfile=${cfg.logFile}"
           ;
-          # generate a config from mutableSettings if it's set (which includes
-          # the immutable settings attrset) otherwise generate it directly
-          # from settings. Then determine the location to link/copy the config
-          # file. klippers working dir if it's
-          # mutable.
-          generateConfig = format.generate "klipper.cfg";
+          # if mutableConfig is set generate a config directly from settings or
+          # configFile. Otherwise generate a mutableConfig from mutableSettings
+          # and include configFile and the a generated config file from 
+          # settings.
+          generateConfig = input: format.generate "klipper.cfg" (flattenConfig input);
           printerConfigFile =
-            if (cfg.mutableSettings != null)
-            then generateConfig cfg.mutableSettings
-            else if (cfg.settings != null)
-            then generateConfig cfg.settings
-            else cfg.configFile;
-          printerConfigPath =
-            if mutable
-            then append mutableConfigFolder "printer.cfg"
-            else printerConfigFile;
+            if (cfg.mutableConfig)
+            then
+              (
+                if (cfg.configFile != null)
+                then cfg.configFile
+                else generateConfig cfg.settings
+              )
+            else
+              generateConfig (
+                (optionalAttrs (cfg.settings != null)
+                  { "include ${cfg.immutableConfigPath}" = { }; })
+                // cfg.mutableSettings
+              );
+          printerConfigPath = cfg.mutableConfigFolder + "/printer.cfg";
         in
         {
           description = "Klipper 3D Printer Firmware";
           wantedBy = [ "multi-user.target" ];
           after = [ "network.target" ];
-          preStart = "mkdir -p ${cfg.mutableConfigFolder}"
-            + optionalString mutable ''
+          preStart = ''
+            mkdir -p ${cfg.mutableConfigFolder}
             [ -e ${printerConfigPath} ] || {
               cp ${printerConfigFile} ${printerConfigPath}
               chmod ug+w ${printerConfigPath}
             }
           '';
 
-          serviceConfig = {
+          serviceConfig = baseServiceConfig // {
             ExecStart = "${cfg.package}/bin/klippy ${klippyArgs} ${printerConfigPath}";
             RuntimeDirectory = "klipper";
-            StateDirectory = "klipper";
             SupplementaryGroups = [ "dialout" ];
             WorkingDirectory = "${cfg.package}/lib";
             OOMScoreAdjust = "-999";
@@ -233,13 +330,7 @@ in
             IOSchedulingClass = "realtime";
             IOSchedulingPriority = 0;
             UMask = "0002";
-          } // (if cfg.user != null then {
-            Group = cfg.group;
-            User = cfg.user;
-          } else {
-            DynamicUser = true;
-            User = "klipper";
-          });
+          };
         };
 
       environment.systemPackages =
@@ -265,13 +356,91 @@ in
             (filterAttrs (mcu: firmware: cfg.firmwares."${mcu}".enableKlipperFlash) firmwares);
         in
         [ klipper-genconf ] ++ firmwareFlasher ++ attrValues firmwares;
+
+      systemd.tmpfiles.settings.klipper = {
+        "${cfg.immutableConfigPath}"."L+".argument =
+          "${format.generate "klipper-immutable.cfg" (flattenConfig cfg.settings)}";
+      };
     }
 
-    (mkIf (cfg.mutableSettings != null && cfg.settings != null) {
-      services.klipper.mutableSettings = {
-        "include ${format.generate "klipper-fixed.cfg" settings}" = { };
-      };
+    (mkIf (cfg.configFile != null) {
+      services.klipper.settings.include = [ cfg.configFile ];
     })
+
+    (mkIf cfg.apiServer.enable (
+      let
+        apiConfig = cfg.apiServer;
+        dataFolder = "/var/lib/klipper";
+        moonrakerConfigFile = format.generate "moonraker.cfg" apiConfig.settings;
+      in
+      mkMerge [
+        {
+          systemd = {
+            services.moonraker = {
+              description = "Moonraker, an API web server for Klipper";
+              wantedBy = [ "multi-user.target" ];
+              wants = [ "klipper.service" ];
+              after = [ "network.target" "klipper.service" ];
+              path = [ pkgs.iproute2 ];
+              serviceConfig = baseServiceConfig // {
+                ExecStart = "${pkgs.moonraker}/bin/moonraker"
+                  + " --datapath ${dataFolder}"
+                  + " --configfile ${moonrakerConfigFile}";
+                WorkingDirectory = "${dataFolder}";
+              };
+            };
+          };
+
+          services.klipper = {
+            apiServer.settings = {
+              server = {
+                host = apiConfig.address;
+                port = apiConfig.port;
+                klippy_uds_address = cfg.apiSocket;
+              };
+              machine = {
+                validate_service = false;
+              };
+            };
+
+            # to make moonraker happy
+            mutableConfigFolder = mkDefault "${dataFolder}/config";
+          };
+        }
+
+
+        (mkIf apiConfig.allowSystemControl {
+          security.polkit.extraConfig =
+            let
+              polkitAllow = { user, ... }@subject: { id, ... }@action:
+                let
+                  actionFilters = [ "action.id == ${id}" ]
+                    ++ (mapAttrsToList
+                    (name: value: ''action.lookup("${name}") == "${value}"'')
+                    (removeAttrs action [ "id" ])
+                  );
+                in
+                ''
+                  polkit.addRule(function(action. subject) {
+                    if (subject.user == "${user}" && ${
+                      concatStringsSep " && " actionFilters
+                    })
+                      { return polkit.Results.YES; }
+                    })
+                '';
+              moonrakerAllow = polkitAllow { user = cfg.user; };
+            in
+            concatLines [
+              (moonrakerAllow { id = "org.freedesktop.login1.power-off"; })
+              (moonrakerAllow { id = "org.freedesktop.login1.power-off-multiple-sessions"; })
+              (moonrakerAllow { id = "org.freedesktop.login1.reboot"; })
+              (moonrakerAllow { id = "org.freedesktop.login1.reboot-multiple-sessions"; })
+              (moonrakerAllow { id = "org.freedesktop.systemd1.manage-units"; unit = "klipper.service"; })
+              (moonrakerAllow { id = "org.freedesktop.systemd1.manage-units"; unit = "moonraker.service"; })
+            ];
+        })
+      ]
+    ))
 
     (mkIf cfg.octoprintIntegration {
       services.klipper = {
